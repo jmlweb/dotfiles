4b1bc60dc403872e02feff35e08a84e79c83722c
José Manuel Lucas <josemanuel@jmlweb.es>
2019-12-08 01:11:33 +0100

    Adding spaceship prompt

--- mac-setup.sh
+++ mac-setup.sh
@@ -27,6 +27,9 @@ brew install zsh zsh-completions
 chsh -s /bin/zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
 ##################
 # NODE USING NVM #
 ##################
